e7c7f8ad1184c290e5a29071abdc0b69374fa26a
Nikhil Nagar <nagarn603@gmail.com>
2026-02-14 00:27:24 +0530

    Add button component and  imports to hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
 import { BackgroundGradient } from "@/components/landing/background-gradient";
+import CtaSection from "@/components/landing/cta-section";
+import FeaturesSection from "@/components/landing/features-section";
+import HeroSection from "@/components/landing/hero-section";
+import HowItWorksSection from "@/components/landing/how-it-works-section";
+import PricingSection from "@/components/landing/pricing-section";
+import { MotionDiv } from "@/components/ui/motion-div";
+
+
+
 export default function Home() {
   return (
     <div className="relative min-h-screen ">
       <BackgroundGradient />
+
+
+      <div className="relative z-10">
+
+        <HeroSection />
+
+
+      </div>
+
     </div>
   );
 }
---
+++ components/landing/hero-section.tsx
@@ -0,0 +1,6 @@
+import { RocketIcon, SparkleIcon, ZapIcon } from "lucide-react";
+import { HeroGradient } from "./background-gradient";
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+
