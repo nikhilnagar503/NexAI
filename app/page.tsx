bb9acdb558e793bc6a9ef190b581608c0b19c4c9
Nikhil Nagar <nagarn603@gmail.com>
2026-02-14 11:01:30 +0530

    hero-section addeed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import { BackgroundGradient } from "@/components/landing/background-gradient";
-import CtaSection from "@/components/landing/cta-section";
-import FeaturesSection from "@/components/landing/features-section";
+// import CtaSection from "@/components/landing/cta-section";
+// import FeaturesSection from "@/components/landing/features-section";
 import HeroSection from "@/components/landing/hero-section";
-import HowItWorksSection from "@/components/landing/how-it-works-section";
-import PricingSection from "@/components/landing/pricing-section";
-import { MotionDiv } from "@/components/ui/motion-div";
+// import HowItWorksSection from "@/components/landing/how-it-works-section";
+// import PricingSection from "@/components/landing/pricing-section";
+// import { MotionDiv } from "@/components/ui/motion-div";
 
 
 
@@ -12,7 +12,6 @@ export default function Home() {
   return (
     <div className="relative min-h-screen ">
       <BackgroundGradient />
-
 
       <div className="relative z-10">
 
